c7da183d17a2b1898c1cddc6830d09ea5e1062f1
L412 <skylarprill@gmail.com>
2018-12-07 17:21:15 -0800

    Write sessions_controller and application_controller

---
+++ benchbnb/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController 
+  # before_action :ensure_session_token
+  # do we still need this here since we're on a single page app?
+
+  def create
+    
+  end
+  
+  def destroy
+    
+  end
+end
--- benchbnb/app/controllers/application_controller.rb
+++ benchbnb/app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    user = User.find_by_session_token(session[:session_token])
+    user ? user : nil
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def logout
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+  
+  def login(user)
+    session[:session_token] = user.reset_session_token
+    # where do we redirect here?
+  end
+  
+  def require_logged_in
+    unless current_user
+      render json: { base: ['invalid credentials'] }, status: 401
+    end
+  end
 end
